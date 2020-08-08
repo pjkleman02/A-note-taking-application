dd7c33ed9c7b479dede1ed1897d4fde267365533
Patrick <patrickkleman02@gmail.com>
2020-08-07 22:11:24 -0500

    added allowed hosts

--- notetaker/settings.py
+++ notetaker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^2qe4nj3lvs=n3(19o#5+(mz)dt76^ohl!ognqti#=#vb%3r(t'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['patricksnotetakingapp','localhost']
 
 
 # Application definition
