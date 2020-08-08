33585c996f1f18f45fce892a2f20f6dae2135f02
Patrick <patrickkleman02@gmail.com>
2020-08-07 22:43:32 -0500

    set debug

--- notetaker/settings.py
+++ notetaker/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 #SECRET_KEY = '^2qe4nj3lvs=n3(19o#5+(mz)dt76^ohl!ognqti#=#vb%3r(t'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['patricksnotetakingapp.herokuapp.com','localhost']
 
